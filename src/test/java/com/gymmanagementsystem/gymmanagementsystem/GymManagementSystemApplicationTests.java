23816ac6284a88deaca7def2e7b02321e4094d35
yashbim <ybimsara03@gmail.com>
2024-01-23 01:28:53 +0530

    Generated and downloaded from spring initializer

---
+++ src/test/java/com/gymmanagementsystem/gymmanagementsystem/GymManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymmanagementsystem.gymmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
