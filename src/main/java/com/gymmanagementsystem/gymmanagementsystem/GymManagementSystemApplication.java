1bf6dbbeb88ca0897143091134cf8994212738ac
yashbim <ybimsara03@gmail.com>
2024-01-23 01:43:05 +0530

    Initial Commit

--- src/main/java/com/gymmanagementsystem/gymmanagementsystem/GymManagementSystemApplication.java
+++ src/main/java/com/gymmanagementsystem/gymmanagementsystem/GymManagementSystemApplication.java
@@ -8,6 +8,7 @@ public class GymManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GymManagementSystemApplication.class, args);
+		System.out.println("hello springboot");
 	}
 
 }
