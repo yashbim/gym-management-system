33de84c74e45f81e0edeff6f3ca0c901c3f3ce1d
yashbim <ybimsara03@gmail.com>
2024-01-28 17:50:28 +0530

    removed unwanted line

--- src/main/java/com/gymmanagementsystem/gymmanagementsystem/GymManagementSystemApplication.java
+++ src/main/java/com/gymmanagementsystem/gymmanagementsystem/GymManagementSystemApplication.java
@@ -8,8 +8,8 @@ public class GymManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GymManagementSystemApplication.class, args);
-		System.out.println("hello springboot");
-		System.out.println("Made on LEGION");
+		System.out.println("Made on LEGION 5");
 	}
+
 
 }
