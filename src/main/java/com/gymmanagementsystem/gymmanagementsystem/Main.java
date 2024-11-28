a6b201c46ea44a32d4a78cadbadd178af8639336
yashbim <ybimsara03@gmail.com>
2024-11-29 00:44:13 +0530

    Renamed class to Main

--- src/main/java/com/gymmanagementsystem/gymmanagementsystem/Main.java
+++ src/main/java/com/gymmanagementsystem/gymmanagementsystem/Main.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GymManagementSystemApplication {
-
+public class Main {
 	public static void main(String[] args) {
-		SpringApplication.run(GymManagementSystemApplication.class, args);
-		System.out.println("Made on LEGION 5");
+		SpringApplication.run(Main.class, args);
+		System.out.println("Made with Linux");
 	}
 
 
